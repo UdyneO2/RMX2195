1a987d01ff67fea59b0727b394d52045444b131e
nurfinzmuhamad14 <mnurdiansyah105@gmail.com>
2025-07-22 20:49:00 +0700

    drop OF_SUPPORT_ALL_BLOCK_OTA_UPDATES

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,7 +37,7 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
-        export TARGET_DEVICE_ALT="RMX2195,RECE4244,RMX2101"
+	export TARGET_DEVICE_ALT="RMX2195,RECE4244,RMX2101"
  	export ALLOW_MISSING_DEPENDENCIES=true
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
@@ -54,32 +54,35 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
-        export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
+    export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
 	export OF_PATCH_AVB20=1
-        export FOX_DELETE_AROMAFM=1
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+	export FOX_DELETE_AROMAFM=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 	export FOX_USE_NANO_EDITOR=1
 	export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
-        export OF_FBE_METADATA_MOUNT_IGNORE=1
-        export OF_ADVANCED_SECURITY=1
-        export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-        
-
-        # OTA
-        export OF_KEEP_DM_VERITY=1
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_SUPPORT_OZIP_DECRYPTION=1
+	export OF_FBE_METADATA_MOUNT_IGNORE=1
+	export OF_ADVANCED_SECURITY=1
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+      
+	# OTA
+	export OF_KEEP_DM_VERITY=1
+	export OF_SUPPORT_OZIP_DECRYPTION=1
+	# dispense with the entire OTA menu
+	export OF_DISABLE_OTA_MENU=1
+	# no additional check for MIUI props
+	export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
 	# Screen settings
-        export OF_SCREEN_H=1600
-        export OF_STATUS_H=80
-        export OF_STATUS_INDENT_LEFT=48
-        export OF_STATUS_INDENT_RIGHT=48
+	export OF_SCREEN_H=1600
+	export OF_STATUS_H=80
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
 	export OF_CLOCK_POS=1
 
 	# R12.1 Settings
 	export FOX_MAINTAINER_PATCH_VERSION="5"
 	export OF_MAINTAINER="mnrdnn."
 	export FOX_VARIANT="UI2"
+	export FOX_BUILD_TYPE="Beta"
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
